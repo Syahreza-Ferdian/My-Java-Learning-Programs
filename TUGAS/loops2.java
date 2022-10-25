a8f4c714b2497f99df08ebfeade8c846276b463d
Syahreza <syahrezafistiferdian32@gmail.com>
2022-10-25 21:08:51 +0700

    Add Loops2 for exercise

---
+++ TUGAS/loops2.java
@@ -0,0 +1,10 @@
+package TUGAS;
+
+public class loops2 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 5555; i++){
+            System.out.printf("Kelas E nih bos %d\n", i);
+        }
+    }
+    
+}
