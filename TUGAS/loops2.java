24d1588e15602be3f4388d8c4b0573a5dc173388
Syahreza <syahrezafistiferdian32@gmail.com>
2022-10-27 00:33:55 +0700

    Update

--- TUGAS/loops2.java
+++ TUGAS/loops2.java
@@ -2,8 +2,8 @@ package TUGAS;
 
 public class loops2 {
     public static void main(String[] args) {
-        for(int i = 0; i <= 5555; i++){
-            System.out.printf("Kelas E nih bos %d\n", i);
+        for(int i = 0; i <= 10; i++){
+            System.out.printf("Testing %d\n", i);
         }
     }
     
