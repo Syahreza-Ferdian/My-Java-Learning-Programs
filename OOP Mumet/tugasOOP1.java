d74c07c0ca082a20b92eada333a936c8e0dc96e7
Syahreza <syahrezafistiferdian32@gmail.com>
2023-02-22 19:50:06 +0700

    Add OOP Section

---
+++ OOP Mumet/tugasOOP1.java
@@ -0,0 +1,51 @@
+// public class tugasOOP1 {
+//     public static void main(String[] args) {
+//         BangunDatar lingkaran1 = new Lingkaran(7);
+//         BangunDatar segiempat1 = new SegiEmpat(5, 6);
+
+//         System.out.printf("Luas lingkaran tersebut adalah %f\n", lingkaran1.hitungLuas());
+//         System.out.println("Luas, keliling, dan panjang diagonal segiempat tersebut: ");
+//         System.out.printf("Luas\t\t: %f\nKeliling\t: %f\nDiagonal\t: %f", segiempat1.hitungLuas(), segiempat1.hitungKeliling(), segiempat1.hitungDiagonal());
+//     }
+// }
+
+// class BangunDatar{
+//     public double hitungKeliling(){
+//         return 0;
+//     }
+//     public double hitungLuas(){
+//         return 0;
+//     }
+//     public double hitungDiagonal(){
+//         return 0;
+//     }
+// }
+
+// class Lingkaran extends BangunDatar{
+//     double jariJari;
+    
+//     public Lingkaran(double jariJari){
+//         this.jariJari = jariJari;
+//     }
+//     public double hitungLuas(){
+//         return Math.PI * jariJari * jariJari;
+//     }
+// }
+
+// class SegiEmpat extends BangunDatar{
+//     int panjang, lebar;
+
+//     public SegiEmpat(int panjang, int lebar){
+//         this.panjang = panjang;
+//         this.lebar = lebar;
+//     }
+//     public double hitungLuas(){
+//         return panjang * lebar;
+//     }
+//     public double hitungKeliling(){
+//         return 2 * (panjang + lebar);
+//     }
+//     public double hitungDiagonal(){
+//         return Math.sqrt(Math.pow(panjang, 2) + Math.pow(lebar, 2));
+//     }
+// }
---
+++ OOP Mumet/tugasOOP1_Alt.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class tugasOOP1_Alt {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Masukkan panjang jari jari lingkaran: ");
+        Lingkaran lingkaran1 = new Lingkaran(scanner.nextDouble());
+        System.out.print("Masukkan panjang dan lebar segiempat: ");
+        SegiEmpat segiEmpat1 = new SegiEmpat(scanner.nextInt(), scanner.nextInt());
+
+        System.out.println("\nLuas lingkaran\t: "+lingkaran1.hitungLuas());
+        System.out.println("\nKeliling Segiempat\t: "+segiEmpat1.hitungKeliling());
+        System.out.println("Luas Segiempat\t\t: "+segiEmpat1.hitungLuas());
+        System.out.println("Diagonal\t\t: "+segiEmpat1.hitungDiagonal());
+    }
+}
+class Lingkaran{
+    double jariJari;
+    public Lingkaran(double jariJari){
+        this.jariJari = jariJari;
+    }
+    public double hitungLuas(){
+        return Math.PI * jariJari * jariJari;
+    }
+}
+class SegiEmpat{
+    int panjang, lebar;
+    public SegiEmpat(int panjang, int lebar){
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    public int hitungLuas(){
+        return panjang * lebar;
+    }
+    public int hitungKeliling(){
+        return 2 * (panjang + lebar);
+    }
+    public double hitungDiagonal(){
+        return Math.sqrt(Math.pow(panjang, 2) + Math.pow(lebar, 2));
+    }
+}
