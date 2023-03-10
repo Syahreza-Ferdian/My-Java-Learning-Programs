ca51fa30aec471fbd641d4d056a7578b9191d4d6
Syahreza <syahrezafistiferdian32@gmail.com>
2023-03-10 13:23:07 +0700

    Update

--- buatNgetest2.java
+++ buatNgetest2.java
@@ -1,6 +1,19 @@
 public class buatNgetest2 {
     public static void main(String[] args) {
+        Mobil mobil1 = new Mobil();
+        mobil1.setNopol("N 111 GA");
+        System.out.println(mobil1.getNopol());
         
     }
     
+}
+class Mobil{
+    private String nopol;
+
+    public void setNopol(String s){
+        nopol = s;
+    }
+    public String getNopol(){
+        return nopol;
+    }
 }
